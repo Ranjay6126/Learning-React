aba616eb79a9d1016b515bd002e4d7a84632d87e
Ranjay pandit prajapati <6126ranjayprajapati@gmail.com>
2024-12-12 23:53:58 +0530

    State-lifting done 😊

---
+++ State-lifting/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from './Components/Card'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div > 
+
+    <Card name="Ranjay Bhai"/>
+
+   </div>
+  )
+}
+
+export default App
---
+++ State-lifting/src/Components/Card.jsx
@@ -0,0 +1,11 @@
+
+
+const Card = (props) => {
+  return (
+    <div >
+      {props.name}
+    </div>
+  )
+}
+
+export default Card
